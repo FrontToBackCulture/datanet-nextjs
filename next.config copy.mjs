d023c24792f5f6d32cd509a6c47607c4710d48f7
melvin@thinkval.com <melvin@thinkval.com>
2022-09-08 09:07:03 +0800

    hide variables

--- next.config copy.mjs
+++ next.config copy.mjs
@@ -6,6 +6,10 @@ import webpack from 'webpack';
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  webpack: (config) => {
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+    return config;
+  },
   env: {
     DEV_API: 'http://localhost:3002s',
     PRODUCTION_API: 'https://zone-assets-api.vercel.app',
