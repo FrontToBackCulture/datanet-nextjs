991524d6d73108109c824ee9f38e6bba6b487c47
melvin@thinkval.com <melvin@thinkval.com>
2022-09-09 13:34:41 +0800

    extract id from array

--- pages/list/items/index.js
+++ pages/list/items/index.js
@@ -101,7 +101,7 @@ export default function PromotionItemsPage() {
           console.log('Metric is an array: ', mD[0][metricKey]);
           merged.push({
             ...sD[i],
-            ...mD.find((itmInner) => itmInner[0][metricKey][0] === sD[i][staticKey]),
+            ...mD.find((itmInner) => itmInner[metricKey][0] === sD[i][staticKey]),
           });
         } else {
           console.log('Metric is not an array: ', mD[0][metricKey]);
