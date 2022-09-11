3e38b2851ddba917ddf114e969a0effe861071c2
melvin@thinkval.com <melvin@thinkval.com>
2022-09-11 13:57:59 +0800

    remove everything

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ import {
 // ----------------------------------------------------------------------
 
 export default function HomePage() {
-  // console.log(process.env.NEXT_PUBLIC_MINIAPP_VALHOST);
   return (
     <Page title="The starting point for your next project">
       <HomeHero />
