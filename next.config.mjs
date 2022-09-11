cfb36f552a9a3f7a7bb0fb8b7b32c7c536a40c02
melvin@thinkval.com <melvin@thinkval.com>
2022-09-11 12:03:00 +0800

    add back next.config stuff

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   env: {
+    DEV_API: 'http://localhost:3002',
+    PRODUCTION_API: 'https://zone-assets-api.vercel.app',
+    GOOGLE_API: '',
     MINIAPP_VALHOST: process.env.MINIAPP_VALHOST,
   },
   images: {
