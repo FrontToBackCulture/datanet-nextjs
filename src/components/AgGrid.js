e9c82d9142a59495d35d057233c310afa202a1dd
melvin@thinkval.com <melvin@thinkval.com>
2022-09-11 08:26:24 +0800

    try again

--- src/components/AgGrid.js
+++ src/components/AgGrid.js
@@ -181,8 +181,8 @@ export default function AGGrid({ rowD, type, fieldConf, fullConf, entity }) {
     }
     if (rowD && fullConf && entity) {
       // console.log('AG GRID USE: ', fullConf, entity);
-      console.log('1st object 1st attribute:', rowD[0][Object.keys(rowD[0])[0]]);
       if (rowD && rowD.length > 0 && rowD[0][Object.keys(rowD[0])[0]]) {
+        console.log('1st object 1st attribute:', rowD[0][Object.keys(rowD[0])[0]]);
         let colDefs = [];
         if (fieldConf) {
           const fields2Show = Object.keys(fieldConf);
