a47005b062116f15d3c22e91c9fcf075721f9dfb
melvin@thinkval.com <melvin@thinkval.com>
2022-09-11 08:36:17 +0800

    again

--- src/components/AgGrid.js
+++ src/components/AgGrid.js
@@ -241,6 +241,7 @@ export default function AGGrid({ rowD, type, fieldConf, fullConf, entity }) {
           setColumnDefs(colDefs);
         }
       }
+      console.log('See what is in rowD:', rowD);
       console.log('See what is in rowData:', rowData);
       setRowData(rowD);
     }
