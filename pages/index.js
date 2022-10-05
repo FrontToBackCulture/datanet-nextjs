b44d253d6b4097d44b4c0700e7aa654e0dd45fc2
Arthur <arthur@thinkval.com>
2022-10-05 14:11:58 +0800

    Simplify index page

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,13 @@
-// react
-import React, { useState, useEffect } from 'react'
-// auth
+import React from 'react'
 import { useUser } from '@auth0/nextjs-auth0'
-// layouts
 import Layout from '../src/layouts'
-// components
 import { Page } from '../src/components'
-// sections
 import { HomeHero } from '../src/sections/home'
 
-// ----------------------------------------------------------------------
-
 export default function HomePage() {
-  const { user, error, isLoading } = useUser()
-  const [userDomain, setUserDomain] = useState()
+  const { user } = useUser()
 
-  useEffect(() => {
-    if (user) {
-      const regex = /@(\w+)/g
-      let result = user.email.match(regex)[0]
-      result = result.substring(1, result.length)
-      switch (result) {
-        case 'saladstop':
-          setUserDomain('saladstop')
-          break
-        case 'thinkval':
-          setUserDomain('thinkval')
-          break
-        case 'kctsoya':
-          setUserDomain('kctsoya')
-          break
-        case 'demo':
-          setUserDomain('demo')
-          break
-        default:
-          break
-      }
-    }
-  }, [user])
+  const userDomain = user?.email.match(/@(\w+)/g)[0]?.slice(1)
 
   return (
     <Page title="The starting point for your next project">
