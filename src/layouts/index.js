a841c28e615182319d4ed82ca0137e9712afd5e4
Arthur <arthur@thinkval.com>
2022-10-05 14:18:50 +0800

    Simplify the layout component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,36 +1,15 @@
-// react
 import { createContext, useContext, useState } from 'react'
-import PropTypes from 'prop-types'
-// next
 import dynamic from 'next/dynamic'
-// nav,header,footer
+
 const Header = dynamic(() => import('./header/Header'), { ssr: false })
-const HeaderSimple = dynamic(() => import('./header/HeaderSimple'), { ssr: false })
 const Footer = dynamic(() => import('./footer/Footer'), { ssr: false })
 const FooterSimple = dynamic(() => import('./footer/FooterSimple'), { ssr: false })
 
 import { DomainProvider } from '../contexts/DomainProvider'
-// ----------------------------------------------------------------------
-
-Layout.propTypes = {
-  children: PropTypes.node,
-  disabledFooter: PropTypes.bool,
-  disabledHeader: PropTypes.bool,
-  simpleFooter: PropTypes.bool,
-  simpleHeader: PropTypes.bool,
-  transparentHeader: PropTypes.bool,
-}
 
 const DomainContext = createContext()
 
-export default function Layout({
-  children,
-  transparentHeader,
-  disabledHeader,
-  disabledFooter,
-  simpleHeader,
-  simpleFooter,
-}) {
+export default function Layout({ children, simpleFooter }) {
   const [selectedDomain, setSelectedDomain] = useState()
   const header2Layout = (domain) => {
     setSelectedDomain(domain)
@@ -38,23 +17,15 @@ export default function Layout({
 
   return (
     <>
-      {disabledHeader ? null : (
-        <>
-          {simpleHeader ? (
-            <HeaderSimple transparent={transparentHeader} />
-          ) : (
-            <Header transparent={transparentHeader} header2Layout={header2Layout} />
-          )}
-        </>
-      )}
+      <Header header2Layout={header2Layout} />
+
       <DomainProvider value={selectedDomain}>{children}</DomainProvider>
 
-      {disabledFooter ? null : <>{simpleFooter ? <FooterSimple /> : <Footer />}</>}
+      {simpleFooter ? <FooterSimple /> : <Footer />}
     </>
   )
 }
 
-// Export useContext Hook.
 export function useDomainContext() {
   return useContext(DomainContext)
 }
