09136ca49abdf2c56cafe1e33dd8c0b58c4c1e8f
Arthur <arthur@thinkval.com>
2022-10-06 15:37:44 +0800

    Simplify the header

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react'
-import PropTypes from 'prop-types'
+import React, { useContext } from 'react'
 import NextLink from 'next/link'
 import {
   Box,
@@ -20,64 +19,28 @@ import { Logo, Label, Iconify } from '../../components'
 import { IconButtonAnimate } from '../../components/animate'
 import { NavMobile, NavDesktop } from '../nav'
 import { ToolbarStyle } from './HeaderToolbarStyle'
-import { selectConfig, selectDomain } from '../../utils/selectScript'
+import { selectConfig } from '../../utils/selectScript'
 import contentDeliveryNetwork from '@iconify/icons-carbon/content-delivery-network'
-import { DomainContext } from '../../contexts/DomainProvider'
-
-Header.propTypes = {
-  transparent: PropTypes.bool,
-}
+import { DomainContext, ROOT_DOMAIN, useUserDomain } from '../../contexts/DomainProvider'
+import { useRouter } from 'next/router'
+import Typography from '../../theme/overrides/Typography'
 
 export default function Header() {
   const { user } = useUser()
-  const [userDomain, setUserDomain] = useState()
-  const [userEmailDomain, setUserEmailDomain] = useState()
-  const [conf, setConf] = useState()
 
   const selectedDomain = useContext(DomainContext)
 
-  const navConfig = (conf ?? []).map(({ title, code }) => ({
+  const userDomain = useUserDomain()
+
+  const navConf = selectConfig(selectedDomain)?.getConfig('navConfig') ?? []
+
+  const navConfig = navConf.map(({ title, code }) => ({
     title,
     path: Routes.list.domainCode(selectedDomain, code),
     code,
   }))
 
   const isDesktop = useResponsive('up', 'md')
-
-  const handleDomainChange = (event) => {
-    setUserDomain(event.target.value)
-  }
-
-  useEffect(() => {
-    if (user) {
-      let domain
-      const regex = /@(\w+)/g
-      let result = user.email.match(regex)[0]
-      result = result.substring(1, result.length)
-      setUserEmailDomain(result)
-      if (!selectedDomain) {
-        if (result != 'thinkval') {
-          domain = selectDomain(result)
-        } else {
-          if (!userDomain) {
-            domain = 'thinkval'
-            setUserEmailDomain('thinkval')
-          } else {
-            domain = userDomain
-          }
-        }
-      } else {
-        domain = selectedDomain
-      }
-      setUserDomain(domain)
-      getConfig(domain)
-    }
-  }, [user, userDomain])
-
-  const getConfig = (domain) => {
-    let config2used = selectConfig(domain).getConfig('navConfig')
-    setConf(config2used)
-  }
 
   return (
     <AppBar position="relative" sx={{ boxShadow: 0, bgcolor: 'transparent' }}>
@@ -113,70 +76,68 @@ export default function Header() {
           <Box sx={{ flexGrow: 1 }} />
 
           <Stack spacing={2} direction="row" alignItems="center">
-            {!user && (
+            {userDomain === ROOT_DOMAIN && (
+              <NextLink href={{ pathname: Routes.admin }} passHref>
+                <IconButtonAnimate color="inherit">
+                  <Iconify icon={contentDeliveryNetwork} sx={{ width: 20, height: 20 }} />
+                </IconButtonAnimate>
+              </NextLink>
+            )}
+
+            {user && <DomainSelector />}
+
+            {user ? (
+              <NextLink href="/api/auth/logout" prefetch={false} passHref>
+                <Button color="inherit" variant="outlined">
+                  Logout
+                </Button>
+              </NextLink>
+            ) : (
               <NextLink href="/api/auth/login" prefetch={false} passHref>
                 <Button color="inherit" variant="outlined">
                   Login
                 </Button>
               </NextLink>
             )}
-
-            {user && userEmailDomain != 'thinkval' && (
-              <>
-                {userDomain} &nbsp;&nbsp;
-                <Divider orientation="vertical" sx={{ height: 24 }} />
-                <NextLink href="/api/auth/logout" prefetch={false} passHref>
-                  <Button color="inherit" variant="outlined">
-                    Logout
-                  </Button>
-                </NextLink>
-              </>
-            )}
-            {user && userEmailDomain == 'thinkval' && (
-              <>
-                <NextLink
-                  href={{
-                    pathname: Routes.admin,
-                  }}
-                  passHref
-                >
-                  <IconButtonAnimate color="inherit">
-                    <Iconify icon={contentDeliveryNetwork} sx={{ width: 20, height: 20 }} />
-                  </IconButtonAnimate>
-                </NextLink>
-                <Box sx={{ minWidth: 120 }}>
-                  <FormControl fullWidth size="small">
-                    <InputLabel id="demo-simple-select-label">Domain</InputLabel>
-                    <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      value={userDomain}
-                      label="Domain"
-                      onChange={handleDomainChange}
-                    >
-                      <MenuItem value="demo">Demo</MenuItem>
-                      <MenuItem value="thinkval">ThinkVAL</MenuItem>
-                      <MenuItem value="saladstop">Salad Stop!</MenuItem>
-                      <MenuItem value="kctsoya">KCT Soya</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Box>
-                &nbsp;&nbsp;
-                <Divider orientation="vertical" sx={{ height: 24 }} />
-                <NextLink href="/api/auth/logout" prefetch={false} passHref>
-                  <Button color="inherit" variant="outlined">
-                    Logout
-                  </Button>
-                </NextLink>
-              </>
-            )}
           </Stack>
 
           {!isDesktop && user && (
-            <NavMobile navConfig={navConfig} sx={{ ml: 1 }} userDomain={userDomain} />
+            <NavMobile navConfig={navConfig} sx={{ ml: 1 }} userDomain={selectedDomain} />
           )}
         </Container>
       </ToolbarStyle>
     </AppBar>
+  )
+}
+
+const DomainSelector = () => {
+  const userDomain = useUserDomain()
+  const selectedDomain = useContext(DomainContext)
+
+  const router = useRouter()
+
+  return (
+    <>
+      <Box sx={{ minWidth: 120 }}>
+        {userDomain === ROOT_DOMAIN ? (
+          <FormControl fullWidth size="small">
+            <InputLabel id="demo-simple-select-label">Domain</InputLabel>
+            <Select
+              label="Domain"
+              value={selectedDomain}
+              onChange={(e) => router.push(`/${e.target.value}`)}
+            >
+              <MenuItem value="demo">Demo</MenuItem>
+              <MenuItem value="thinkval">ThinkVAL</MenuItem>
+              <MenuItem value="saladstop">Salad Stop!</MenuItem>
+              <MenuItem value="kctsoya">KCT Soya</MenuItem>
+            </Select>
+          </FormControl>
+        ) : (
+          <Typography>{userDomain}</Typography>
+        )}
+      </Box>
+      <Divider orientation="vertical" sx={{ height: 24 }} />
+    </>
   )
 }
