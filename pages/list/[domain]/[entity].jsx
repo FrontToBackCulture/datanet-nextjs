147e2ada5b68ce2b17b4190ed1de43d7af6e81f8
Arthur <arthur@thinkval.com>
2022-10-11 17:47:08 +0800

    Move the async calls to getServerSideProps

--- pages/list/[domain]/[entity].jsx
+++ pages/list/[domain]/[entity].jsx
@@ -11,21 +11,18 @@ import { selectConfig, selectLocalDataSource } from '../../../src/utils/selectSc
 import { DomainContext, ROOT_DOMAIN, useUserDomain } from '../../../src/contexts/DomainProvider'
 import Header from '../../../src/layouts/header/Header'
 
-export default function ListPage() {
+export default function ListPage({ rowData, conf }) {
   const { user } = useUser()
-  const [rowData, setRowData] = useState([])
-  const [rawData, setRawData] = useState({})
 
   const router = useRouter()
   const { domain, entity } = router.query
 
   const userDomain = useUserDomain()
 
-  const selectedDomain = userDomain === ROOT_DOMAIN ? domain : userDomain
+  // const selectedDomain = userDomain === ROOT_DOMAIN ? domain : userDomain
+  const selectedDomain = domain
 
   const domainConf = selectConfig(selectedDomain)
-
-  const conf = domainConf?.getConfig(entity)
 
   const title = domainConf?.getConfig('navConfig')?.find((route) => route.code === entity)?.title
 
@@ -34,88 +31,10 @@ export default function ListPage() {
   // - reset row data to blank
   // - push event to GA4
   useEffect(() => {
-    setRowData([])
     if (process.env.NEXT_PUBLIC_CONFIGURATION === 'production') {
       gtag.event({ action: 'screenview', category: entity, label: user.email, value: 1 })
     }
   }, [router.query, userDomain])
-
-  // once config have been extracted, process based on config instructions
-  useEffect(() => {
-    ;(async () => {
-      if (conf) {
-        const { dataSources } = conf
-
-        const allData = {}
-        for (const dataSet of Object.keys(dataSources)) {
-          const { domain, queryID, contentType, name } = dataSources[dataSet]
-          const data = await getDataFromVAL(queryID, domain, contentType, entity, true)
-          allData[name] = data
-        }
-        setRawData(allData)
-      }
-    })()
-  }, [conf])
-
-  useEffect(() => {
-    ;(async () => {
-      if (conf && entity && rawData && rawData[`${entity}Static`] && rawData[`${entity}Metrics`]) {
-        const { dataSources, variablesMetrics, listFields, detailFields } = conf
-        const { staticSource, metricSource, trendSource } = dataSources
-        const staticKey = dataSources.staticSource.key
-        const metricKey = dataSources.metricSource.key
-        const trendKey = dataSources.trendSource.key
-
-        const mergeParams = {
-          arr1: rawData[`${entity}Static`],
-          arr2: rawData[`${entity}Metrics`],
-          arr1Key: staticKey,
-          arr2Key: metricKey,
-          domain: userDomain,
-          dataType: entity,
-        }
-
-        // merge static and metric
-        let mergeStaticMetricData = await dataNetMerge(mergeParams)
-        mergeStaticMetricData = mergeStaticMetricData.data
-        rawData.mergeStaticMetric = mergeStaticMetricData
-
-        const performCalcRequiredData = {}
-        performCalcRequiredData.mergeStaticMetric = mergeStaticMetricData
-        performCalcRequiredData[trendSource.name] = rawData[trendSource.name]
-
-        const performCalcParams = {
-          data: performCalcRequiredData,
-          conf,
-          domain: userDomain,
-          dataType: entity,
-        }
-
-        let performCalcData
-        if ((rawData, mergeStaticMetricData)) {
-          performCalcData = await dataNetPerformCalc(performCalcParams)
-          performCalcData = performCalcData.data
-
-          setRowData(performCalcData)
-        }
-      }
-    })()
-  }, [rawData])
-
-  // get static and metric data from VAL
-  const getDataFromVAL = async (id, dom, contentType, dataType, cache) => {
-    if (process.env.NEXT_PUBLIC_CONFIGURATION === 'development') {
-      return selectLocalDataSource(contentType, dataType, dom)
-    }
-    const valJobs = await readVAL({
-      queryID: id,
-      domain: dom,
-      contentType,
-      dataType,
-      cache,
-    })
-    return valJobs.data
-  }
 
   return (
     <DomainContext.Provider value={selectedDomain}>
@@ -129,4 +48,87 @@ export default function ListPage() {
       </Stack>
     </DomainContext.Provider>
   )
+}
+
+// get static and metric data from VAL
+const getDataFromVAL = async (id, dom, contentType, dataType, cache) => {
+  if (process.env.NEXT_PUBLIC_CONFIGURATION === 'development') {
+    return selectLocalDataSource(contentType, dataType, dom)
+  }
+  const valJobs = await readVAL({
+    queryID: id,
+    domain: dom,
+    contentType,
+    dataType,
+    cache,
+  })
+  return valJobs.data
+}
+
+export const getServerSideProps = async ({ params }) => {
+  const { domain, entity } = params
+
+  const selectedDomain = domain
+
+  const domainConf = selectConfig(selectedDomain)
+
+  const conf = domainConf?.getConfig(entity)
+
+  const { dataSources } = conf
+
+  const rawData = {}
+  for (const dataSet of Object.keys(dataSources)) {
+    const { domain, queryID, contentType, name } = dataSources[dataSet]
+    rawData[name] = await getDataFromVAL(queryID, domain, contentType, entity, true)
+  }
+
+  let rowData
+
+  if (conf && entity && rawData && rawData[`${entity}Static`] && rawData[`${entity}Metrics`]) {
+    const { dataSources, variablesMetrics, listFields, detailFields } = conf
+    const { staticSource, metricSource, trendSource } = dataSources
+    const staticKey = dataSources.staticSource.key
+    const metricKey = dataSources.metricSource.key
+    const trendKey = dataSources.trendSource.key
+
+    const mergeParams = {
+      arr1: rawData[`${entity}Static`],
+      arr2: rawData[`${entity}Metrics`],
+      arr1Key: staticKey,
+      arr2Key: metricKey,
+      domain: selectedDomain,
+      dataType: entity,
+    }
+
+    // merge static and metric
+    let mergeStaticMetricData = await dataNetMerge(mergeParams)
+    mergeStaticMetricData = mergeStaticMetricData.data
+    rawData.mergeStaticMetric = mergeStaticMetricData
+
+    const performCalcRequiredData = {}
+    performCalcRequiredData.mergeStaticMetric = mergeStaticMetricData
+    performCalcRequiredData[trendSource.name] = rawData[trendSource.name]
+
+    const performCalcParams = {
+      data: performCalcRequiredData,
+      conf,
+      domain: selectedDomain,
+      dataType: entity,
+    }
+
+    let performCalcData
+    if ((rawData, mergeStaticMetricData)) {
+      performCalcData = await dataNetPerformCalc(performCalcParams)
+      performCalcData = performCalcData.data
+
+      rowData = performCalcData
+    }
+  }
+
+  return {
+    props: {
+      rowData,
+      conf,
+    },
+  }
 }
