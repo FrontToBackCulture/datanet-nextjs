ddb1a7b9cb7f7d32da59fa989c86f81ae00dea2e
Arthur <arthur@thinkval.com>
2022-10-12 14:39:26 +0800

    Move the async calls to getServerSideProps in the second page

--- pages/list/[domain]/[entity]/[id].jsx
+++ pages/list/[domain]/[entity]/[id].jsx
@@ -31,181 +31,38 @@ function TabPanel(props) {
   )
 }
 
-export default function PromotionItemPage() {
+export default function PromotionItemPage({
+  conf,
+  tab1,
+  tab1Name,
+  multiSeriesChannelData,
+  uniqueChannels,
+  overviewName,
+  item,
+  dataRows,
+  chartData,
+}) {
   const { user } = useUser()
-  const [value, setValue] = useState(0)
-  const [item, setItem] = useState()
-  const [dataRows, setDataRows] = useState([])
-  const [chartData, setChartData] = useState([])
-  const [multiSeriesChannelData, setMultiSeriesChannelData] = useState([])
-  const [uniqueChannels, setUniqueChannels] = useState([])
-  const [tab1, setTab1] = useState(false)
-  const [overviewName, setOverviewName] = useState()
-  const [tab1Name, setTab1Name] = useState()
+  const [tabId, setTabId] = useState(0)
 
   const isDesktop = useResponsive('up', 'md')
   const router = useRouter()
 
-  const { domain, entity, id } = router.query
+  const { domain, entity } = router.query
 
   const userDomain = useUserDomain()
 
   const selectedDomain = userDomain === ROOT_DOMAIN ? domain : userDomain
 
-  const domainConf = selectConfig(selectedDomain)
-
-  const conf = domainConf?.getConfig(entity)
-
-  // tab value changes
-  const handleChange = (event, newValue) => {
-    setValue(newValue)
+  const switchTab = (_event, newTabId) => {
+    setTabId(newTabId)
   }
 
-  // get data from VAL
-  const getDataFromVAL = async (qId, dom, contentType, dataType, cache) => {
-    if (process.env.NEXT_PUBLIC_CONFIGURATION === 'development') {
-      return selectLocalDataSource(contentType, dataType, dom)
+  useEffect(() => {
+    if (user && process.env.NEXT_PUBLIC_CONFIGURATION === 'production') {
+      gtag.event({ action: 'screenview', category: entity, label: user.email, value: 1 })
     }
-    const valJobs = await readVAL({
-      queryID: qId,
-      domain: dom,
-      contentType,
-      dataType,
-      cache,
-    })
-    return valJobs.data
-  }
-
-  useEffect(async () => {
-    if (router.isReady) {
-      if (entity && selectedDomain) {
-        if (user && process.env.NEXT_PUBLIC_CONFIGURATION === 'production') {
-          gtag.event({ action: 'screenview', category: entity, label: user.email, value: 1 })
-        }
-        const { dataSources, variablesMetrics, listFields, detailFields } = conf
-        const { staticSource, metricSource, trendSource } = dataSources
-        const { overview, tab1, tab2 } = detailFields
-        const staticKey = dataSources.staticSource.key
-        const metricKey = dataSources.metricSource.key
-        const trendKey = dataSources.trendSource.key
-
-        if (detailFields.overview) {
-          setOverviewName(detailFields.overview.name)
-        }
-
-        if (detailFields.tab1) {
-          setTab1(true)
-          setTab1Name(detailFields.tab1.name)
-        }
-
-        // iterate thru all the datasources define, cache and extract to UI
-        const allData = {}
-        for (const dataSet of Object.keys(dataSources)) {
-          const { domain, queryID, contentType, name } = dataSources[dataSet]
-          const data = await getDataFromVAL(queryID, domain, contentType, entity, true)
-          allData[name] = data
-        }
-
-        const mergeParams = {
-          arr1: allData[`${entity}Static`],
-          arr2: allData[`${entity}Metrics`],
-          arr1Key: staticKey,
-          arr2Key: metricKey,
-          domain: selectedDomain,
-          dataType: entity,
-        }
-
-        // merge static and metric
-        let mergeStaticMetricData = await dataNetMerge(mergeParams)
-        mergeStaticMetricData = mergeStaticMetricData.data
-        allData.mergeStaticMetric = mergeStaticMetricData
-
-        const performCalcRequiredData = {}
-        performCalcRequiredData.mergeStaticMetric = mergeStaticMetricData
-        performCalcRequiredData[trendSource.name] = allData[trendSource.name]
-
-        const performCalcParams = {
-          data: performCalcRequiredData,
-          conf,
-          domain: selectedDomain,
-          dataType: entity,
-        }
-
-        let performCalcData = await dataNetPerformCalc(performCalcParams)
-        performCalcData = performCalcData.data
-
-        // filter the selected item from the newly merged data with trend
-        const filtered = performCalcData.find((merge) => merge[staticKey] === id)
-
-        // for item hero -----------------------------------------
-        // fetch the fields to show from static and metric
-        const fields2ShowList = Object.keys(listFields)
-        const itemObj = {}
-        fields2ShowList.forEach((field2Show) => {
-          const variableMetric = conf.variablesMetrics[listFields[field2Show].variablesMetrics]
-          itemObj[field2Show] = filtered[variableMetric.sourceColumn]
-        })
-        setItem(itemObj)
-        // for item hero -----------------------------------------
-
-        // fetch the fields to show at the table level for overview aka landing page of each item   --------------------
-        const fields2ShowDetail = Object.keys(overview.table)
-        const itemArray2 = []
-        fields2ShowDetail.forEach((field2Show) => {
-          const variableMetric = conf.variablesMetrics[overview.table[field2Show].variablesMetrics]
-          itemArray2.push({
-            name: variableMetric.headerName,
-            value: filtered[variableMetric.sourceColumn],
-          })
-        })
-        setDataRows(itemArray2)
-        // fetch the fields to show at the table level for overview aka landing page of each item   --------------------
-
-        // fetch the data for the chart for overview aka landing page    --------------------
-        let filteredChart = []
-        if (allData[dataSources[overview.chart.dataSource].name].length > 0) {
-          filteredChart = selectObject(
-            allData[dataSources[overview.chart.dataSource].name],
-            trendKey,
-            metricKey,
-            id
-          )
-          setChartData(filteredChart)
-        }
-        // fetch the data for the chart  for overview aka landing page  --------------------
-
-        // group by channel performance   --------------------
-        if (detailFields.tab1) {
-          const { tab1 } = detailFields
-          const { chart } = tab1
-          const { queryID, domain, key, groupKey, groupPeriodKey, valueKey, contentType } =
-            dataSources[chart.dataSource]
-          const cpd = allData[dataSources[chart.dataSource].name]
-
-          // filter data specific to this id
-          const cpdFiltered = cpd.filter((row) => row[key] === id)
-
-          const convert2MultiSeriesParams = {
-            data: cpdFiltered,
-            conf,
-            domain: selectedDomain,
-            dataType: entity,
-            contentType,
-            itemId: id,
-          }
-
-          // convert data into multiseries
-          let multiSeriesData = await dataNetConvert2MultiSeries(convert2MultiSeriesParams)
-          const arrayUniqueByKey = multiSeriesData.data.uniqueKey
-          multiSeriesData = multiSeriesData.data.multiSeriesData
-          setUniqueChannels(arrayUniqueByKey)
-          setMultiSeriesChannelData(multiSeriesData)
-        }
-
-        // group by channel performance   --------------------
-      }
-    }
-  }, [router.isReady, selectedDomain])
+  }, [user, entity])
 
   if (!item) {
     return <LoadingScreen />
@@ -224,23 +81,23 @@ export default function PromotionItemPage() {
                   <Box sx={{ width: '100%', padding: '0px' }}>
                     <Box sx={{ borderBottom: 1, borderColor: 'divider', padding: '0px' }}>
                       <Tabs
-                        value={value}
+                        value={tabId}
                         scrollButtons="auto"
                         variant="scrollable"
                         allowScrollButtonsMobile
-                        onChange={handleChange}
+                        onChange={switchTab}
                       >
                         <Tab label={overviewName} />
                         {tab1 && <Tab label={tab1Name} />}
                       </Tabs>
                     </Box>
-                    <TabPanel value={value} index={0} sx={{ padding: '0px' }}>
+                    <TabPanel value={tabId} index={0} sx={{ padding: '0px' }}>
                       <br />
                       <SimpleAreaChart conf={conf} chartData={chartData} tab="overview" />
                       <DataTable job={dataRows} conf={conf} tabType="overview" />
                     </TabPanel>
                     {tab1 && (
-                      <TabPanel value={value} index={1}>
+                      <TabPanel value={tabId} index={1}>
                         <br />
                         <MultiLineSeriesChart
                           conf={conf}
@@ -262,17 +119,17 @@ export default function PromotionItemPage() {
                   <Box sx={{ width: '100%', padding: '0px' }}>
                     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                       <Tabs
-                        value={value}
+                        value={tabId}
                         scrollButtons="auto"
                         variant="scrollable"
                         allowScrollButtonsMobile
-                        onChange={handleChange}
+                        onChange={switchTab}
                       >
                         <Tab label={overviewName} />
                         {tab1 && <Tab label={tab1Name} />}
                       </Tabs>
                     </Box>
-                    <TabPanel value={value} index={0}>
+                    <TabPanel value={tabId} index={0}>
                       <br />
                       <SimpleAreaChart conf={conf} chartData={chartData} tab="overview" />
                       <Stack sx={{ marginTop: '10px' }} direction="row" spacing={2}>
@@ -281,7 +138,7 @@ export default function PromotionItemPage() {
                       </Stack>
                     </TabPanel>
                     {tab1 && (
-                      <TabPanel value={value} index={1}>
+                      <TabPanel value={tabId} index={1}>
                         <br />
                         <MultiLineSeriesChart
                           conf={conf}
@@ -303,4 +160,151 @@ export default function PromotionItemPage() {
       </Stack>
     </DomainContext.Provider>
   )
+}
+
+const getDataFromVAL = async (qId, dom, contentType, dataType, cache) => {
+  if (process.env.NEXT_PUBLIC_CONFIGURATION === 'development') {
+    return selectLocalDataSource(contentType, dataType, dom)
+  }
+  const valJobs = await readVAL({
+    queryID: qId,
+    domain: dom,
+    contentType,
+    dataType,
+    cache,
+  })
+  return valJobs.data
+}
+
+export const getServerSideProps = async ({ params }) => {
+  const { domain, entity, id } = params
+
+  const selectedDomain = domain
+
+  const domainConf = selectConfig(selectedDomain)
+
+  const conf = domainConf?.getConfig(entity)
+
+  const { dataSources, listFields, detailFields } = conf
+
+  const { trendSource } = dataSources
+  const { overview } = detailFields
+  const staticKey = dataSources.staticSource.key
+  const metricKey = dataSources.metricSource.key
+  const trendKey = dataSources.trendSource.key
+
+  const overviewName = detailFields?.overview?.name
+
+  // iterate thru all the datasources define, cache and extract to UI
+  const allData = {}
+  for (const dataSet of Object.keys(dataSources)) {
+    const { domain, queryID, contentType, name } = dataSources[dataSet]
+    const data = await getDataFromVAL(queryID, domain, contentType, entity, true)
+    allData[name] = data
+  }
+
+  const mergeParams = {
+    arr1: allData[`${entity}Static`],
+    arr2: allData[`${entity}Metrics`],
+    arr1Key: staticKey,
+    arr2Key: metricKey,
+    domain: selectedDomain,
+    dataType: entity,
+  }
+
+  // merge static and metric
+  let mergeStaticMetricData = await dataNetMerge(mergeParams)
+  mergeStaticMetricData = mergeStaticMetricData.data
+  allData.mergeStaticMetric = mergeStaticMetricData
+
+  const performCalcRequiredData = {}
+  performCalcRequiredData.mergeStaticMetric = mergeStaticMetricData
+  performCalcRequiredData[trendSource.name] = allData[trendSource.name]
+
+  const performCalcParams = {
+    data: performCalcRequiredData,
+    conf,
+    domain: selectedDomain,
+    dataType: entity,
+  }
+
+  const performCalcData = (await dataNetPerformCalc(performCalcParams)).data
+
+  // filter the selected item from the newly merged data with trend
+  const filtered = performCalcData.find((merge) => merge[staticKey] === id)
+
+  // for item hero -----------------------------------------
+  // fetch the fields to show from static and metric
+  let item = {}
+  Object.keys(listFields).forEach((field2Show) => {
+    const variableMetric = conf.variablesMetrics[listFields[field2Show].variablesMetrics]
+    item[field2Show] = filtered[variableMetric.sourceColumn]
+  })
+
+  // for item hero -----------------------------------------
+
+  // fetch the fields to show at the table level for overview aka landing page of each item   --------------------
+  const fields2ShowDetail = Object.keys(overview.table)
+  const dataRows = fields2ShowDetail.map((field2Show) => {
+    const variableMetric = conf.variablesMetrics[overview.table[field2Show].variablesMetrics]
+    return {
+      name: variableMetric.headerName,
+      value: filtered[variableMetric.sourceColumn],
+    }
+  })
+  // fetch the fields to show at the table level for overview aka landing page of each item   --------------------
+
+  // fetch the data for the chart for overview aka landing page    --------------------
+  let chartData = []
+  if (allData[dataSources[overview.chart.dataSource].name].length > 0) {
+    chartData = selectObject(
+      allData[dataSources[overview.chart.dataSource].name],
+      trendKey,
+      metricKey,
+      id
+    )
+  }
+  // fetch the data for the chart  for overview aka landing page  --------------------
+
+  // group by channel performance   --------------------
+  let multiSeriesChannelData = null
+  let uniqueChannels = null
+  if (detailFields.tab1) {
+    const { tab1 } = detailFields
+    const { chart } = tab1
+    const { key, contentType } = dataSources[chart.dataSource]
+    const cpd = allData[dataSources[chart.dataSource].name]
+
+    // filter data specific to this id
+    const cpdFiltered = cpd.filter((row) => row[key] === id)
+
+    const convert2MultiSeriesParams = {
+      data: cpdFiltered,
+      conf,
+      domain: selectedDomain,
+      dataType: entity,
+      contentType,
+      itemId: id,
+    }
+
+    // convert data into multiseries
+    const multiSeriesData = await dataNetConvert2MultiSeries(convert2MultiSeriesParams)
+    const arrayUniqueByKey = multiSeriesData.data.uniqueKey
+    multiSeriesChannelData = multiSeriesData.data.multiSeriesData
+    uniqueChannels = arrayUniqueByKey
+  }
+
+  return {
+    props: {
+      conf,
+      tab1: !!detailFields.tab1,
+      tab1Name: detailFields?.tab1?.name ?? null,
+      multiSeriesChannelData,
+      uniqueChannels,
+      overviewName,
+      item,
+      dataRows,
+      chartData,
+    },
+  }
 }
