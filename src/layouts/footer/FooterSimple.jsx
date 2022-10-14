abc4401f3cb99ae2ba7c2708dc37e0ca81f9db18
Arthur <arthur@thinkval.com>
2022-10-14 10:47:51 +0800

    Improve the footer padding

--- src/layouts/footer/FooterSimple.jsx
+++ src/layouts/footer/FooterSimple.jsx
@@ -4,7 +4,7 @@ import { Logo } from '../../components'
 
 export default function FooterSimple() {
   return (
-    <Stack direction="row" justifyContent="center" pb={{ xs: 2, md: 4 }}>
+    <Stack direction="row" justifyContent="center" py={{ xs: 2, md: 4 }}>
       <Logo isSimple />
       <Typography color="text.secondary">© 2022. All rights reserved</Typography>
     </Stack>
