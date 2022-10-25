cef918598a3d16b431f3cf3881437633a5930c4e
Arthur <arthur@thinkval.com>
2022-10-25 14:36:48 +0800

    Simplify selectLocalDataSource

--- src/utils/selectScript.js
+++ src/utils/selectScript.js
@@ -14,35 +14,5 @@ export function selectObject(arr, checkKey, selectKey, item) {
   return result
 }
 
-export function selectLocalDataSource(contentType, dataType, domain) {
-  let outletJsonData
-  let productJsonData
-  let promotionJsonData
-  let localJsonData
-  if (dataType === 'outlet' && localData[domain][dataType][contentType]) {
-    // outletJsonData = outletData[contentType].getData();
-    outletJsonData = localData[domain][dataType][contentType].getData()
-  }
-  if (dataType === 'product' && localData[domain][dataType][contentType]) {
-    // productJsonData = productData[contentType].getData();
-    productJsonData = localData[domain][dataType][contentType].getData()
-  }
-  if (dataType === 'promotion' && localData[domain][dataType][contentType]) {
-    // promotionJsonData = productData[contentType].getData();
-    promotionJsonData = localData[domain][dataType][contentType].getData()
-  }
-  switch (dataType) {
-    case 'outlet':
-      localJsonData = outletJsonData
-      break
-    case 'product':
-      localJsonData = productJsonData
-      break
-    case 'promotion':
-      localJsonData = promotionJsonData
-      break
-    default:
-      break
-  }
-  return localJsonData
-}
+export const selectLocalDataSource = (contentType, dataType, domain) =>
+  localData[domain][dataType][contentType]?.getData()
