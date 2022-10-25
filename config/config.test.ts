7c1bf7f6c07a5a0e0ac058baa97394d63c504c34
Arthur <arthur@thinkval.com>
2022-10-25 11:38:58 +0800

    Add a test on navConfig

--- config/config.test.ts
+++ config/config.test.ts
@@ -11,6 +11,12 @@ domains.forEach((domain) => {
     Object.keys(entities).forEach((entity) => {
       const variablesMetricsKeys = Object.keys(entities[entity].variablesMetrics)
 
+      describe('All entity keys in navConfig shoud be defined', () => {
+        confProd[domain].navConfig.forEach((navItem) => {
+          expect(Object.keys(confProd[domain].entities)).toContain(navItem.code)
+        })
+      })
+
       describe(`Entity: ${entity}`, () => {
         test('All variable metrics listed in listFields should be defined', () => {
           Object.values(entities[entity].listFields).forEach(({ variablesMetrics }) => {
@@ -27,7 +33,7 @@ domains.forEach((domain) => {
         })
 
         if (entities[entity].detailFields.tab1?.table) {
-          test('All variable metrics listed in detailFields should be defined (tab1)', () => {
+          test('All variable metrics listed in detailFields should be defined (tab1 table)', () => {
             Object.values(entities[entity].detailFields.tab1.table).forEach(
               ({ variablesMetrics }) => {
                 expect(variablesMetricsKeys).toContain(variablesMetrics)
